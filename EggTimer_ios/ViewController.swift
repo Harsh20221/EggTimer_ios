308d89ca4e2ef851da8f028cd2825b041ecd1bca
Harsh <harsh20221@gmail.com>
2024-07-19 20:08:42 +0530

    Fixed Egg sound logic

--- EggTimer_ios/ViewController.swift
+++ EggTimer_ios/ViewController.swift
@@ -5,16 +5,30 @@
 //  Created by Angela Yu on 08/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-    let softtime=30;
+    let softtime=3;
     let mediumtime=42;
     let hardtime=72;
     
     var timer = Timer(); //# This timer variable will fix the issue of multiple timer running at same time
-    
+    var player: AVAudioPlayer?
+
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
    
     
     @IBOutlet var ProgressbarView: UIProgressView!;
@@ -22,6 +36,7 @@ class ViewController: UIViewController {
     @IBOutlet var Titleoutlet: UILabel!
    
     @IBAction func EggController(_ sender: UIButton) {
+     
         Titleoutlet.text="How do you like your eggs?"
         var timepassed : Int=0;
         timer.invalidate() //* we'll invalidate current timer if we press the button again to avaoid multiple timers running at same time issue
@@ -44,11 +59,14 @@ class ViewController: UIViewController {
                         
                             timepassed += 1
             ProgressbarView.progress=Float(Float(timepassed)/Float(totaltime))
+            ///This will Keep Track of the progress , This equation will determine the progress in ProgressBar
                           print(timepassed, "Seconds")
                         
                           if(timepassed==totaltime){
                                     timer.invalidate()
-                              Titleoutlet.text="Done"
+                              Titleoutlet.text="Done";
+                              playSound();
+                            
                                 }
                        }
     }
